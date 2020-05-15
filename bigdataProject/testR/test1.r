66209f1f641d1aabc07db27961592970e1123ad9
gentacle <gentacle00@gmail.com>
2020-05-15 21:10:57 +0900

    start r project

---
+++ bigdataProject/testR/test1.r
@@ -0,0 +1,27 @@
+# 초기화
+# x <- y
+# x -> y
+
+# 실행
+# 드래그 + crtl + enter
+
+# 변수var
+# test2.  가능
+# .test   가능
+# .2test  불가
+# 2test   불가  
+# test%   불가
+# _test   불가
+
+
+a <- 1
+a+5
+x <- c(1,2,3,4,2,1,2,3)
+x
+
+
+v <- c(2,3,5,8,11)
+plot(v, type="o")
+
+
+l <- list("pizza", 3, 119.2)
